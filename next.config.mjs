abe2515f970053973c6b108c0790acb23b864941
Berk YILDIZ <berkxathena@gmail.com>
2026-07-04 06:22:42 +0300

    fix(routing): use trailingSlash and try_files for next.js static export Nginx 403 error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   // Disable image optimization since we are using static export
   images: {
     unoptimized: true,
